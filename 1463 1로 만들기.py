fff48e82b51ecf3c58868a617a4fac78e3fef2e6
2zm00 <ljm010301@naver.com>
2024-10-07 20:12:03 +0900

    .\1463 1로 만들기.py

--- 1463 1로 만들기.py
+++ 1463 1로 만들기.py
@@ -1,25 +1,14 @@
 import sys
 
-
-
-
-
-
-def calc(n):
-    count = 0
-    while n > 1:
-        if n % 2 == 0:
-            n = n // 2
-        elif n % 3 == 0:
-            n = n // 3
-        else:
-            n = n - 1
-        count += 1
-    return count
-
+def make_one(n):
+    dp = [0] * (n + 1)
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+    return dp[n]
 
 n = int(sys.stdin.readline())
-result=calc(n)
-
-
-print(result)
+print(make_one(n))
