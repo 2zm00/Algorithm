8b23411e1a1c16b97cf29e79f0811632760a55e4
2zm00 <ljm010301@naver.com>
2024-11-29 17:43:02 +0900

    32326 Conveyor belt sushi.py

---
+++ 32326 Conveyor belt sushi.py
@@ -0,0 +1,14 @@
+def calculate_sushi_cost(red, green, blue):
+    cost_red = red * 3      # 빨간 접시 비용
+    cost_green = green * 4  # 초록 접시 비용
+    cost_blue = blue * 5    # 파란 접시 비용
+    total_cost = cost_red + cost_green + cost_blue
+    return total_cost
+
+# 입력 받기
+red = int(input())    # 빨간 접시 수
+green = int(input())  # 초록 접시 수
+blue = int(input())   # 파란 접시 수
+
+# 결과 출력
+print(calculate_sushi_cost(red, green, blue))
