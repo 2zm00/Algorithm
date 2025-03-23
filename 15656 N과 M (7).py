23bfcecc8ac5e1c618bd7bc247a842bdf27e86b7
2zm00 <ljm010301@naver.com>
2025-03-23 23:02:05 +0900

    15656 N과 M (7).py

--- 15656 N과 M (7).py
+++ 15656 N과 M (7).py
@@ -5,14 +5,14 @@ N, M = map(int, input().split())
 num = sorted(map(int, input().split()))
 lst = []
 
-def dfs(start, depth):
-	if len(depth) == M:
+def dfs():
+	if len(lst) == M:
 		print(" ".join(map(str, lst)))
 		return
 	
-	for i in range(start, N+1):
-		lst.append(num[i])
-		dfs(i+1)
+	for i in num:
+		lst.append(i)
+		dfs()
 		lst.pop()
 
-dfs(0)
+dfs()
