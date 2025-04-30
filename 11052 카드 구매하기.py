5054ff9bdd12861c79cc2c322dda3e39088976cf
2zm00 <ljm010301@naver.com>
2025-04-30 23:43:11 +0900

    11052 카드 구매하기.py

---
+++ 11052 카드 구매하기.py
@@ -0,0 +1,32 @@
+# 최대한 비싸게 사서 카드갯수를 충족하는 카드팩을 사야함
+# 꼭 카드팩들 카드 합은 N과 같아야함
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+card = list(map(int, input().split()))
+
+dp = [0]*(n+1)
+
+# [0][0][0][0]..n개
+# dp는 점화식 만드는 문제
+
+dp[0]=0
+
+# 힌트 썼음..
+# ~~~~~~~~ 
+# 카드가 [1]일때 가장 비싸게 살 수 있는 최댓값을 구하면서 갱신 최댓값 ~ [n]일때 가장 비싸게 살 수 있는 최댓값을 구하면서 갱신
+
+"""
+n=5야
+그럼 i=4일때 j=5(n)-4(i)=1 이어야 함 
+그럼 i=3일때 j=5(n)-3(i)=2 이어야 함 
+
+"""
+for i in range(1, n+1):
+	for j in range(1, i+1):
+		dp[i] = max(dp[i], dp[i-j] + card[j-1])
+
+print(dp[n])
+
