21a5f0f339f67704b24565445f4508d04eb6264c
2zm00 <ljm010301@naver.com>
2025-05-13 21:50:25 +0900

    43105 정수삼각형.py

---
+++ 43105 정수삼각형.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+    floor = len(triangle) -1
+    
+    while floor>0:
+        for i in range(floor):
+            triangle[floor-1][i] += max(triangle[floor][i], triangle[floor][i+1])
+        floor-=1
+    
+    answer= triangle[0][0]
+
+    return answer
